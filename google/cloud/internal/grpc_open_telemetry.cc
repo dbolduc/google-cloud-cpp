dd616f7b07c8cba398723c548600a102c9598a12
dbolduc <dbolduc@google.com>
2022-09-09 05:04:22 -0400

    MakeGrpcSpan (gRPC context propagation)

---
+++ google/cloud/internal/grpc_open_telemetry.cc
@@ -0,0 +1,67 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/internal/grpc_open_telemetry.h"
+#include <grpcpp/client_context.h>
+#ifdef GOOGLE_CLOUD_CPP_HAVE_OPEN_TELEMETRY
+#include <opentelemetry/context/propagation/global_propagator.h>
+#include <opentelemetry/trace/provider.h>
+#include <opentelemetry/trace/tracer_provider.h>
+#endif  // GOOGLE_CLOUD_CPP_HAVE_OPEN_TELEMETRY
+
+namespace google {
+namespace cloud {
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
+namespace internal {
+
+#ifdef GOOGLE_CLOUD_CPP_HAVE_OPEN_TELEMETRY
+namespace {
+class GrpcClientCarrier
+    : public opentelemetry::context::propagation::TextMapCarrier {
+ public:
+  explicit GrpcClientCarrier(grpc::ClientContext& context)
+      : context_(context) {}
+
+  opentelemetry::nostd::string_view Get(
+      opentelemetry::nostd::string_view) const noexcept override {
+    return "";
+  }
+
+  void Set(opentelemetry::nostd::string_view key,
+           opentelemetry::nostd::string_view value) noexcept override {
+    context_.AddMetadata(key.data(), value.data());
+  }
+
+  grpc::ClientContext& context_;
+};
+}  // namespace
+
+opentelemetry::nostd::shared_ptr<opentelemetry::trace::Span> MakeGrpcSpan(
+    opentelemetry::nostd::string_view name, grpc::ClientContext& context) {
+  auto span = MakeSpan(name);
+  // TODO(dbolduc): We probably want to factor out the injection part...
+  auto current = opentelemetry::context::RuntimeContext::GetCurrent();
+  GrpcClientCarrier carrier(context);
+  auto prop = opentelemetry::context::propagation::GlobalTextMapPropagator::
+      GetGlobalPropagator();
+  prop->Inject(carrier, current);
+  return span;
+}
+
+#endif  // GOOGLE_CLOUD_CPP_HAVE_OPEN_TELEMETRY
+
+}  // namespace internal
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
+}  // namespace cloud
+}  // namespace google
---
+++ google/cloud/internal/grpc_open_telemetry.h
@@ -0,0 +1,52 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_INTERNAL_GRPC_OPEN_TELEMETRY_H
+#define GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_INTERNAL_GRPC_OPEN_TELEMETRY_H
+
+#include "google/cloud/internal/open_telemetry.h"
+#include "google/cloud/version.h"
+#ifdef GOOGLE_CLOUD_CPP_HAVE_OPEN_TELEMETRY
+#include <opentelemetry/nostd/shared_ptr.h>
+#include <opentelemetry/trace/span.h>
+#include <opentelemetry/trace/tracer_provider.h>
+#endif  // GOOGLE_CLOUD_CPP_HAVE_OPEN_TELEMETRY
+#include <grpcpp/grpcpp.h>
+
+namespace google {
+namespace cloud {
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
+namespace internal {
+
+#ifdef GOOGLE_CLOUD_CPP_HAVE_OPEN_TELEMETRY
+
+/**
+ * Make a span that propagates its context by setting gRPC metadata.
+ *
+ * In order to get visibility into anything happening on the server side, we
+ * need to associate the requests with the spans that we export. We accomplish
+ * this by copying the span's attributes into the grpc::ClientContext's as
+ * metadata.
+ */
+opentelemetry::nostd::shared_ptr<opentelemetry::trace::Span> MakeGrpcSpan(
+    opentelemetry::nostd::string_view name, grpc::ClientContext& context);
+
+#endif  // GOOGLE_CLOUD_CPP_HAVE_OPEN_TELEMETRY
+
+}  // namespace internal
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
+}  // namespace cloud
+}  // namespace google
+
+#endif  // GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_INTERNAL_GRPC_OPEN_TELEMETRY_H
